0a26ee91b3a17a8ad9807c8703db574ae590f08a
kfree23 <krystalfreeman50@gmail.com>
2026-06-10 18:17:38 -0400

    get weather data function using async await and api

--- Weather App/index.js
+++ Weather App/index.js
@@ -24,6 +24,15 @@ weatherForm.addEventListener('submit', async event => {
 
 async function getWeatherData(city) {
 
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const response = await fetch(apiUrl);
+    console.log(response);
+
+    if (!respone) {
+        throw new Error("Could not fetch weather data.")
+    }
+
+    return await response.json();
 }
 
 function displayWeatherInfo(data) {
